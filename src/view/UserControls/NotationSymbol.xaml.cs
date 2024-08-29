85193c78e19d26ff7898fa1541ed5ac5e7d5fd9b
random-randoms <daniil.savinov.05@gmail.com>
2024-08-29 18:19:05 +0300

    feat: add NoteHead

---
+++ src/view/UserControls/NotationSymbol.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Controls;
+using tunelint.model;
+
+namespace tunelint.view.UserControls {
+  /// <summary>
+  /// Interaction logic for NotationSymbol.xaml
+  /// </summary>
+  public partial class NotationSymbol : UserControl {
+    
+
+    private Note? _note;
+    private Clef _clef;
+
+    private NoteHead _noteHead;
+    private NoteStem _noteStem;
+
+    internal Note? Note { 
+      set {
+        _note = value;
+        _noteHead.Note = value;
+      } 
+    }
+
+    internal Clef Clef {
+      set {
+        _clef = value;
+      }
+    }
+
+    public NotationSymbol() {
+      InitializeComponent();
+      _clef = Clef.Treble;
+      _noteHead = new();
+    }
+
+    private void Rebuild() {
+      if (_note == null) {
+        return;
+      }
+
+      var offset = _note.BaseNote.Pitch - _clef.FirstLinePitch;
+    }
+  }
+}
---
+++ src/view/UserControls/NoteHead.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+using tunelint.model;
+
+namespace tunelint.view.UserControls {
+  /// <summary>
+  /// Interaction logic for NoteHead.xaml
+  /// </summary>
+  public partial class NoteHead : UserControl {
+    public enum NoteHeadType {
+      Black,
+      White,
+      Whole,
+      None
+    };
+
+    private static readonly Dictionary<bool, Visibility> _visibility = new() {
+      { true, Visibility.Visible },
+      { false, Visibility.Hidden }
+    };
+
+    private NoteHeadType _type;
+    private Brush _fill;
+
+    internal Note? Note {
+      set {
+        _type = HeadTypeOf(value);
+        Rebuild();
+      }
+    }
+    public Brush Fill {
+      get => _fill;
+      set {
+        _fill = value;
+        BlackHead.Fill = value;
+        WhiteHead.Fill = value;
+        WholeHead.Fill = value;
+      }
+    }
+
+    public NoteHead() {
+      InitializeComponent();
+      _fill = Brushes.Black;
+      _type = NoteHeadType.Black;
+    }
+
+    private void Rebuild() {
+      BlackHead.Visibility = _visibility[_type == NoteHeadType.Black];
+      WhiteHead.Visibility = _visibility[_type == NoteHeadType.White];
+      WholeHead.Visibility = _visibility[_type == NoteHeadType.Whole];
+    }
+
+    private static NoteHeadType HeadTypeOf(Note? note) {
+      if (note == null) return NoteHeadType.None;
+
+      if (note.NoteValue == NoteValue.Half) return NoteHeadType.White;
+
+      if (note.NoteValue.Duration > NoteValue.Half.Duration) return NoteHeadType.Whole;
+
+      return NoteHeadType.Black;
+    }
+  }
+}
