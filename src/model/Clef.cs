b83322c79440c79c659e1d6c04f54bad29f1f5bb
random-randoms <daniil.savinov.05@gmail.com>
2024-08-29 16:32:58 +0300

    feat: add clef

---
+++ src/model/Clef.cs
@@ -0,0 +1,30 @@
+﻿namespace tunelint.model {
+  class Clef : IEquatable<Clef> {
+    private readonly Pitch _firstLinePitch;
+    public Pitch FirstLinePitch => _firstLinePitch;
+
+    public Clef(Pitch firstLinePitch) {
+      _firstLinePitch = firstLinePitch;
+    }
+
+    public static Clef Treble => new(new(2));
+    public static Clef Alto => new(new(-4));
+    public static Clef Bass => new(new(-15));
+
+    public bool Equals(Clef? other)
+      => _firstLinePitch.Equals(other?._firstLinePitch);
+    public static bool operator ==(Clef left, Clef right)
+      => left.Equals(right);
+    public static bool operator !=(Clef left, Clef right)
+      => !left.Equals(right);
+
+    public override bool Equals(object? obj) {
+      if (obj is not Clef) return false;
+
+      return Equals(obj as Clef);
+    }
+
+    public override int GetHashCode() 
+      => _firstLinePitch.GetHashCode();
+  }
+}
---
+++ test/model/ClefTests.cs
@@ -0,0 +1,16 @@
+﻿using tunelint.model;
+
+namespace test.model {
+  internal class ClefTests {
+    private static readonly object[] _equalsCases = [
+      new object[] { new Clef(new(0)), new Clef(new(0)),  true  },
+      new object[] { new Clef(new(0)), new Clef(new(4)),  false },
+      new object[] { Clef.Alto,        new Clef(new(-4)), true  },
+      new object[] { Clef.Treble,      new Clef(new(12)), false }
+    ];
+
+    [TestCaseSource(nameof(_equalsCases))]
+    public void TestEquals(Clef left, Clef Right, bool expected) =>
+      Assert.That(left.Equals(Right), Is.EqualTo(expected));
+  }
+}
