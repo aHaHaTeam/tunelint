c1bf87baf24f7c371cf6c63b01813c9ed9e03c04
random-randoms <daniil.savinov.05@gmail.com>
2024-08-29 15:30:46 +0300

    feat: add note user controls

---
+++ src/view/UserControls/WholeNote.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tunelint.view.UserControls {
+  /// <summary>
+  /// Interaction logic for WholeNote.xaml
+  /// </summary>
+  public partial class WholeNote : UserControl {
+    private Brush _fill;
+
+    public Brush Fill {
+      get => _fill;
+      set {
+        _fill = value;
+        Head.Fill = _fill;
+      }
+    }
+
+    public WholeNote() {
+      InitializeComponent();
+      _fill = Brushes.Black;
+    }
+  }
+}
